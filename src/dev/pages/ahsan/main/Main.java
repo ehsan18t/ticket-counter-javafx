5a022daf3c9237e6fcd3335b0ae638f2157dd297
Ahsan Khan <nobody.ahsan@gmail.com>
2021-12-18 16:56:14 +0600

    made loginScene draggable and some other small tweaks

--- src/dev/pages/ahsan/main/Main.java
+++ src/dev/pages/ahsan/main/Main.java
@@ -4,19 +4,49 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 import java.util.Objects;
 
 public class Main extends Application {
     public static Stage primaryStage;
+    private static double xOffset;
+    private static double yOffset;
 
     @Override
     public void start(Stage stage) throws Exception{
+        Scene scene;
+
+        // Set FXML
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(Config.loginScene)));
         primaryStage = stage;
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/dev/pages/ahsan/login/loginScene.fxml")));
+
+        // Set Title
         primaryStage.setTitle(Config.title + " " + Config.version);
+
+        // Set CSS
         root.getStylesheets().add(Objects.requireNonNull(getClass().getResource(Config.CSS)).toExternalForm());
-        primaryStage.setScene(new Scene(root));
+
+        // Make Stage Transparent
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+
+        scene = new Scene(root);
+
+        // Make Scene Draggable
+        scene.setOnMousePressed(event -> {
+            xOffset = primaryStage.getX() - event.getScreenX();
+            yOffset = primaryStage.getY() - event.getScreenY();
+        });
+        scene.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() + xOffset);
+            primaryStage.setY(event.getScreenY() + yOffset);
+        });
+
+        // Make Scene Transparent
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
