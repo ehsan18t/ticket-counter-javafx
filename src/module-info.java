0d47d3ba55e14f17b25096fe461193c8040a04da
Ahsan Khan <nobody.ahsan@gmail.com>
2022-01-16 16:11:13 +0600

    organized module info

--- src/module-info.java
+++ src/module-info.java
@@ -1,12 +1,20 @@
 module ticket.counter {
+    // Dependency
     requires javafx.fxml;
     requires javafx.controls;
     requires javafx.graphics;
+
+    // Opens
     opens dev.pages.ahsan.main to javafx.fxml;
     opens dev.pages.ahsan.login to javafx.fxml;
     opens dev.pages.ahsan.registration to javafx.fxml;
+    opens dev.pages.ahsan.dashboard to javafx.fxml;
+
+    // Exports
     exports dev.pages.ahsan.main to javafx.graphics;
     exports dev.pages.ahsan.login to javafx.graphics;
     exports dev.pages.ahsan.registration to javafx.graphics;
+    exports dev.pages.ahsan.dashboard to javafx.graphics;
     exports dev.pages.ahsan.utils to javafx.graphics;
+    exports dev.pages.ahsan.user to javafx.graphics;
 }
