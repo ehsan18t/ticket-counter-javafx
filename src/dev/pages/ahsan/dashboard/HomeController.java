4565b6e552e5e79bdc76ee83f1725f51e6de543b
Ahsan Khan <nobody.ahsan@gmail.com>
2022-01-17 12:14:33 +0600

    added application menu

--- src/dev/pages/ahsan/dashboard/HomeController.java
+++ src/dev/pages/ahsan/dashboard/HomeController.java
@@ -1,17 +1,25 @@
 package dev.pages.ahsan.dashboard;
 
+import animatefx.animation.*;
 import dev.pages.ahsan.main.Config;
 import dev.pages.ahsan.main.Main;
 import dev.pages.ahsan.utils.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.effect.Effect;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
+import java.util.Timer;
 
 public class HomeController implements Initializable {
 
@@ -27,15 +35,58 @@ public class HomeController implements Initializable {
     @FXML
     private ImageView btnLogout;
 
+    @FXML
+    private ImageView btnMenu;
+
+    @FXML
+    private AnchorPane menuPane;
+
+    @FXML
+    private AnchorPane mainPaneHome;
+
+    Image image1;
+    Image image2;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        image1 = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/res/img/menu-expand.png")));
+        image2 = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/res/img/017-menu-6.png")));
+
         // set
+        menuPane.setVisible(false);
         txtTitle.setText(Config.title + " " + Config.version);
 
         // Action Event
         btnClose.setOnMouseClicked(this::setBtnCloseAction);
         btnMin.setOnMouseClicked(this::setBtnMinAction);
         btnLogout.setOnMouseClicked(this::btnLogoutAction);
+        btnMenu.setOnMouseClicked(this::btnMenuAction);
+    }
+
+
+    private void btnMenuAction(MouseEvent mouseEvent) {
+        if (menuPane.isVisible()) {
+            btnMenu.setImage(image2);
+            new FadeIn(btnMenu).play();
+            new SlideOutLeft(menuPane).play();
+            Timer t = new java.util.Timer();
+            t.schedule(new java.util.TimerTask() {
+                @Override
+                public void run() {
+                    new Thread(() -> {
+                        mainPaneHome.setEffect(null);
+                        menuPane.setVisible(false);
+                        t.cancel();
+                    }).start();
+                }
+            }, 500);
+        } else {
+            btnMenu.setImage(image1);
+            new FadeIn(btnMenu).play();
+            menuPane.setVisible(true);
+            new SlideInLeft(menuPane).play();
+            mainPaneHome.setEffect(new GaussianBlur(40));
+        }
     }
 
     private void btnLogoutAction(MouseEvent mouseEvent) {
