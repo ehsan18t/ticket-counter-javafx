6c9857bf313bb776993e72ccceae779cd5564eac
Ahsan Khan <nobody.ahsan@gmail.com>
2022-01-19 18:42:09 +0600

    added option to add bus in admin panel

--- src/dev/pages/ahsan/admin/AdminController.java
+++ src/dev/pages/ahsan/admin/AdminController.java
@@ -5,9 +5,15 @@ import animatefx.animation.SlideInLeft;
 import animatefx.animation.SlideOutLeft;
 import dev.pages.ahsan.main.Config;
 import dev.pages.ahsan.main.Main;
+import dev.pages.ahsan.user.Bus;
 import dev.pages.ahsan.utils.Utils;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -16,12 +22,34 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.ResourceBundle;
 import java.util.Timer;
 
 public class AdminController implements Initializable {
+    HashSet<Bus> buses;
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private DatePicker tfDate;
+
+    @FXML
+    private TextField tfFrom;
+
+    @FXML
+    private TextField tfId;
+
+    @FXML
+    private TextField tfTime;
+
+    @FXML
+    private TextField tfTo;
 
     @FXML
     private ImageView btnClose;
@@ -50,6 +78,24 @@ public class AdminController implements Initializable {
     @FXML
     private AnchorPane btnSettings;
 
+    @FXML
+    private TableView<Bus> table;
+
+    @FXML
+    private TableColumn<Bus, String> timeCol;
+
+    @FXML
+    private TableColumn<Bus, String> toCol;
+
+    @FXML
+    private TableColumn<Bus, LocalDate> dateCol;
+
+    @FXML
+    private TableColumn<Bus, String> fromCol;
+
+    @FXML
+    private TableColumn<Bus, Integer> idCol;
+
     Image image1;
     Image image2;
 
@@ -57,6 +103,24 @@ public class AdminController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         image1 = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/res/img/menu-expand.png")));
         image2 = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/res/img/017-menu-6.png")));
+
+        // get data from server
+        try {
+            Main.sendObj.writeObject("getBusList");
+            buses = (HashSet<Bus>) Main.receiveObj.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        // Table
+        idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        fromCol.setCellValueFactory(new PropertyValueFactory<>("from"));
+        toCol.setCellValueFactory(new PropertyValueFactory<>("to"));
+        dateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
+        timeCol.setCellValueFactory(new PropertyValueFactory<>("time"));
+        table.getColumns().addAll(idCol, fromCol, toCol, dateCol, timeCol);
+        table.setItems(getBus());
 
         // set
         menuPane.setVisible(false);
@@ -68,10 +132,35 @@ public class AdminController implements Initializable {
         btnLogout.setOnMouseClicked(this::btnLogoutAction);
         btnMenu.setOnMouseClicked(this::btnMenuAction);
         btnSettings.setOnMouseClicked(this::btnSettingsAction);
+        btnAdd.setOnAction(this::btnAddAction);
 
         System.out.println(" - Logged in as " + Main.user.getName());
         txtUserName.setText(Main.user.getName() + "");
     }
+
+    private void btnAddAction(ActionEvent actionEvent) {
+        try {
+            Main.sendObj.writeObject("addBus");
+        System.out.println("Adding");
+            Bus bus = new Bus(Integer.parseInt(tfId.getText()), tfFrom.getText(), tfTo.getText(), tfDate.getValue(), tfTime.getText());
+            Main.sendObj.writeObject(bus);
+            buses.add(bus);
+            table.getItems().clear();
+            table.setItems(getBus());
+            table.refresh();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // get add buses
+    public ObservableList<Bus> getBus() {
+        ObservableList<Bus> allBus = FXCollections.observableArrayList();
+        allBus.addAll(buses);
+        return allBus;
+    }
+
+
 
     public void btnSettingsAction(MouseEvent mouseEvent) {
         Main.screenController.activate("Settings");
