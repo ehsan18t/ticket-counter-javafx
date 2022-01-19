d0073b7e5fa57f15b92b9ae3bfb096dd18ec52c9
Ahsan Khan <nobody.ahsan@gmail.com>
2022-01-19 15:05:10 +0600

    added admin page to config

--- src/module-info.java
+++ src/module-info.java
@@ -11,6 +11,7 @@ module ticket.counter {
     opens dev.pages.ahsan.registration to javafx.fxml;
     opens dev.pages.ahsan.dashboard to javafx.fxml;
     opens dev.pages.ahsan.settings to javafx.fxml;
+    opens dev.pages.ahsan.admin to javafx.fxml;
     opens lib to javafx.fxml;
 
     // Exports
@@ -18,6 +19,7 @@ module ticket.counter {
     exports dev.pages.ahsan.login to javafx.graphics;
     exports dev.pages.ahsan.registration to javafx.graphics;
     exports dev.pages.ahsan.dashboard to javafx.graphics;
+    exports dev.pages.ahsan.admin to javafx.graphics;
     exports dev.pages.ahsan.settings to javafx.graphics;
     exports dev.pages.ahsan.utils to javafx.graphics;
     exports dev.pages.ahsan.user to javafx.graphics;
