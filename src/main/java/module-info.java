2028f6780fc9c3dc0246ebc8d8b8758c4b6cdedc
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-03-21 21:55:27 +0600

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,35 +1,18 @@
 module ticket.counter {
-    // Dependency
+    // Dependencies
     requires javafx.fxml;
     requires javafx.controls;
     requires javafx.graphics;
     requires AnimateFX;
     requires java.desktop;
 
-    // Opens
+    // exports
     opens dev.pages.ehsan.main to javafx.fxml;
-    opens dev.pages.ehsan.login to javafx.fxml;
-    opens dev.pages.ehsan.registration to javafx.fxml;
-    opens dev.pages.ehsan.dashboard to javafx.fxml;
-    opens dev.pages.ehsan.settings to javafx.fxml;
-    opens dev.pages.ehsan.admin to javafx.fxml;
-    opens dev.pages.ehsan.buy to javafx.fxml;
-    opens dev.pages.ehsan.about to javafx.fxml;
-    opens dev.pages.ehsan.user to javafx.base;
-    opens dev.pages.ehsan.utils to javafx.base;
-    opens lib to javafx.fxml;
-
-    // Exports
-    exports dev.pages.ehsan.main to javafx.graphics;
-    exports dev.pages.ehsan.login to javafx.graphics;
-    exports dev.pages.ehsan.registration to javafx.graphics;
-    exports dev.pages.ehsan.dashboard to javafx.graphics;
-    exports dev.pages.ehsan.admin to javafx.graphics;
-    exports dev.pages.ehsan.settings to javafx.graphics;
-    exports dev.pages.ehsan.buy to javafx.graphics;
-    exports dev.pages.ehsan.about to javafx.graphics;
-    exports dev.pages.ehsan.utils to javafx.graphics;
-    exports dev.pages.ehsan.user to javafx.graphics;
-    exports dev.pages.ehsan.controllers to javafx.graphics;
+    exports dev.pages.ehsan.main;
     opens dev.pages.ehsan.controllers to javafx.fxml;
+    exports dev.pages.ehsan.controllers;
+    opens dev.pages.ehsan.user to javafx.base;
+    exports dev.pages.ehsan.user;
+    opens dev.pages.ehsan.utils to javafx.base;
+    exports dev.pages.ehsan.utils;
 }
